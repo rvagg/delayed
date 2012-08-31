5698d21a54e5a525606e135063505228b46f049a
Rod Vagg <rod@vagg.org>
2012-08-31 16:10:13 +1000

    cumulativeDelayed & ctx argument for all

--- delayed.js
+++ delayed.js
@@ -19,26 +19,41 @@
         return Array.prototype.slice.call(arr, i)
       }
 
-    , delay = function (fn, seconds) {
-        var args = slice(arguments, 2)
+    , delay = function (fn, seconds, ctx) {
+        var args = slice(arguments, 3)
         return setTimeout(function() {
-          fn.apply(fn, args)
+          fn.apply(ctx || null, args)
         }, seconds * 1000)
       }
 
-    , defer = function (fn) {
-        return delay.apply(null, [ fn, deferSeconds ].concat(slice(arguments, 1)))
+    , defer = function (fn, ctx) {
+        return delay.apply(null, [ fn, deferSeconds, ctx ].concat(slice(arguments, 2)))
       }
 
-    , delayed = function (fn, seconds) {
-        var args = slice(arguments, 2)
+    , delayed = function () {
+        var args = slice(arguments)
         return function () {
-          return delay.apply(null, [ fn, seconds ].concat(args).concat(slice(arguments)))
+          return delay.apply(null, args.concat(slice(arguments)))
         }
       }
 
-    , deferred = function (fn) {
-        return delayed.apply(null, [ fn, deferSeconds ].concat(slice(arguments, 1)))
+    , deferred = function (fn, ctx) {
+        return delayed.apply(null, [ fn, deferSeconds, ctx ].concat(slice(arguments, 2)))
+      }
+
+    , cumulativeDelayed = function (fn, seconds, ctx) {
+        var args = slice(arguments, 3)
+          , timeout = null
+
+        return function() {
+          var _args = slice(arguments)
+            , f = function() {
+                return fn.apply(ctx || null, args.concat(_args))
+              }
+          if (timeout != null)
+            clearTimeout(timeout)
+          return timeout = setTimeout(f, seconds * 1000)
+        }
       }
 
     , noConflict = function () {
@@ -48,11 +63,12 @@
 
 
   return {
-      delay      : delay
-    , defer      : defer
-    , delayed    : delayed
-    , deferred   : deferred
-    , noConflict : noConflict
+      delay             : delay
+    , defer             : defer
+    , delayed           : delayed
+    , deferred          : deferred
+    , noConflict        : noConflict
+    , cumulativeDelayed : cumulativeDelayed
   }
 
 }));
--- tests.js
+++ tests.js
@@ -31,20 +31,23 @@ buster.testCase('Delayed', {
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args.length, 0)
+            assert.isNull(spy.thisValues[0])
             done()
           }, 110)
         }
 
       , 'curried arguments': function (done) {
           var spy = this.spy()
+            , ctx = {}
 
-          D.delay(spy, 0.01, 'foo', 'bar')
+          D.delay(spy, 0.01, ctx, 'foo', 'bar')
 
           assert.equals(spy.callCount, 0)
 
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args, [ 'foo', 'bar' ])
+            assert.same(spy.thisValues[0], ctx)
             done()
           }, 20)
         }
@@ -74,20 +77,23 @@ buster.testCase('Delayed', {
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args.length, 0)
+            assert.isNull(spy.thisValues[0])
             done()
           }, 5)
         }
 
       , 'curried arguments': function (done) {
           var spy = this.spy()
+            , ctx = {}
 
-          D.defer(spy, 'foo', 'bar')
+          D.defer(spy, ctx, 'foo', 'bar')
 
           assert.equals(spy.callCount, 0)
 
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args, [ 'foo', 'bar' ])
+            assert.same(spy.thisValues[0], ctx)
             done()
           }, 5)
         }
@@ -119,27 +125,31 @@ buster.testCase('Delayed', {
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args.length, 0)
+            assert.isNull(spy.thisValues[0])
             done()
           }, 110)
         }
 
       , 'curried arguments': function (done) {
           var spy = this.spy()
+            , ctx = {}
 
-          D.delayed(spy, 0.01, 'foo', 'bar')('bang', 'boo')
+          D.delayed(spy, 0.01, ctx, 'foo', 'bar')('bang', 'boo')
 
           assert.equals(spy.callCount, 0)
 
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args, [ 'foo', 'bar', 'bang', 'boo' ])
+            assert.same(spy.thisValues[0], ctx)
             done()
           }, 20)
         }
 
       , 'multiple calls, curried': function (done) {
           var spy = this.spy()
-            , fn = D.delayed(spy, 0.01, 'spicy')
+            , ctx = {}
+            , fn = D.delayed(spy, 0.01, ctx, 'spicy')
 
           fn('foo', 'bar')
 
@@ -148,11 +158,13 @@ buster.testCase('Delayed', {
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args, [ 'spicy', 'foo', 'bar' ])
+            assert.same(spy.thisValues[0], ctx)
             fn('boom', 'bang')
 
             setTimeout(function () {
               assert.equals(spy.callCount, 2)
               assert.equals(spy.secondCall.args, [ 'spicy', 'boom', 'bang' ])
+              assert.same(spy.thisValues[1], ctx)
               done()
             }, 20)
           }, 20)
@@ -170,27 +182,31 @@ buster.testCase('Delayed', {
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args.length, 0)
+            assert.isNull(spy.thisValues[0])
             done()
           }, 5)
         }
 
       , 'curried arguments': function (done) {
           var spy = this.spy()
+            , ctx = {}
 
-          D.deferred(spy, 'foo', 'bar')('bang', 'boo')
+          D.deferred(spy, ctx, 'foo', 'bar')('bang', 'boo')
 
           assert.equals(spy.callCount, 0)
 
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args, [ 'foo', 'bar', 'bang', 'boo' ])
+            assert.same(spy.thisValues[0], ctx)
             done()
           }, 5)
         }
 
       , 'multiple calls, curried': function (done) {
           var spy = this.spy()
-            , fn = D.deferred(spy, 'spicy')
+            , ctx = {}
+            , fn = D.deferred(spy, ctx, 'spicy')
 
           fn('foo', 'bar')
 
@@ -199,14 +215,99 @@ buster.testCase('Delayed', {
           setTimeout(function () {
             assert.equals(spy.callCount, 1)
             assert.equals(spy.firstCall.args, [ 'spicy', 'foo', 'bar' ])
+            assert.same(spy.thisValues[0], ctx)
             fn('boom', 'bang')
 
             setTimeout(function () {
               assert.equals(spy.callCount, 2)
               assert.equals(spy.secondCall.args, [ 'spicy', 'boom', 'bang' ])
+              assert.same(spy.thisValues[1], ctx)
               done()
             }, 5)
           }, 5)
+        }
+    }
+
+  , 'cumulativeDelayed()': {
+        'no-arg 100ms': function (done) {
+          var spy = this.spy()
+
+          D.delayed(spy, 0.1)()
+
+          assert.equals(spy.callCount, 0)
+
+          setTimeout(function () { assert.equals(spy.callCount, 0) }, 50)
+          setTimeout(function () { assert.equals(spy.callCount, 0) }, 75)
+          setTimeout(function () {
+            assert.equals(spy.callCount, 1)
+            assert.equals(spy.firstCall.args.length, 0)
+            assert.isNull(spy.thisValues[0])
+            done()
+          }, 110)
+        }
+
+      , 'curried arguments': function (done) {
+          var spy = this.spy()
+            , ctx = {}
+
+          D.delayed(spy, 0.01, ctx, 'foo', 'bar')('bang', 'boo')
+
+          assert.equals(spy.callCount, 0)
+
+          setTimeout(function () {
+            assert.equals(spy.callCount, 1)
+            assert.equals(spy.firstCall.args, [ 'foo', 'bar', 'bang', 'boo' ])
+            assert.same(spy.thisValues[0], ctx)
+            done()
+          }, 20)
+        }
+
+      , 'multiple calls within same tick, curried': function (done) {
+          var spy = this.spy()
+            , ctx = {}
+            , fn = D.cumulativeDelayed(spy, 0.01, ctx, 'spicy')
+
+          fn('foo1', 'bar1')
+          fn('foo2', 'bar2')
+          fn('foo3', 'bar3')
+
+          assert.equals(spy.callCount, 0)
+
+          setTimeout(function () {
+            assert.equals(spy.callCount, 1)
+            assert.equals(spy.firstCall.args, [ 'spicy', 'foo3', 'bar3' ])
+            assert.same(spy.thisValues[0], ctx)
+            done()
+          }, 20)
+        }
+
+      , 'multiple calls across ticks, curried': function (done) {
+          var spy = this.spy()
+            , ctx = {}
+            , fn = D.cumulativeDelayed(spy, 0.05, ctx, 'spicy')
+
+          fn('foo1', 'bar1')
+
+          assert.equals(spy.callCount, 0)
+
+          setTimeout(function () {
+            assert.equals(spy.callCount, 0)
+            fn('foo2', 'bar2')
+            setTimeout(function () {
+              assert.equals(spy.callCount, 0)
+              fn('foo3', 'bar3')
+              setTimeout(function () {
+                assert.equals(spy.callCount, 0)
+                fn('foo4', 'bar4')
+                setTimeout(function () {
+                  assert.equals(spy.callCount, 1)
+                  assert.equals(spy.firstCall.args, [ 'spicy', 'foo4', 'bar4' ])
+                  assert.same(spy.thisValues[0], ctx)
+                  done()
+                }, 100)
+              }, 30)
+            }, 30)
+          }, 30)
         }
     }
 })
